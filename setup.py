1d088c19f1d3f4e680e9c8f64c7423c76719c1f2
Garvitxx18 <gs2013061@sgsitsindore.in>
2023-10-24 03:31:03 +0530

    Release 3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup( 
 	name="mini-kyber", 
-	version="0.0.2", 
+	version="0.0.3", 
 	author="Garvit Prasad", 
 	author_email="garvitpd789@gmail.com", 
 	packages=["Kyber"], 
